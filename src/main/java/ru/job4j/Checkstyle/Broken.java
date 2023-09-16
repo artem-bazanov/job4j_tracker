d02353a37b0ac65a4efd881f1ef918ae2fdff786
artem-bazanov <atremjava@gmail.com>
2023-09-16 07:59:54 +0300

    6. Правила Checkstyle [#504981 #436258]

--- src/main/java/ru/job4j/Checkstyle/Broken.java
+++ src/main/java/ru/job4j/Checkstyle/Broken.java
@@ -6,7 +6,7 @@ public class Broken {
     private String surname;
     public static final String NEW_VALUE = "";
 
-    public Broken() { }
+    public void broken() { }
 
     public void echo() { }
 
